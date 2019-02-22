ec31a192a08a1d9fea4eb629340f04798e31f1d6
Stephen <chuikam1004@hotmail.com>
2019-02-22 10:52:33 +0800

    Initial Commit

---
+++ BulgyTabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BulgyTabBarController
+//
+//  Created by Stephen Chui on 2019/2/22.
+//  Copyright © 2019 Stephen Chui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
